5d785727f3c7ae1459d05438e2e48d3749703822
taiyo2001 <taiyo2778@gmail.com>
2024-08-30 02:23:41 +0900

    fix: lint

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -5,11 +5,11 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 FILES=(.zshrc .bashrc)
 
 for FILE in "${FILES[@]}"; do
-    GIT_RC="$SCRIPT_DIR/dotfiles/$FILE"
-    HOME_RC="$HOME/$FILE"
+  GIT_RC="$SCRIPT_DIR/dotfiles/$FILE"
+  HOME_RC="$HOME/$FILE"
 
-    echo -e "\n# Load aliases from $GIT_RC" >> "$HOME_RC"
-    echo "source $GIT_RC" >> "$HOME_RC"
+  echo -e "\n# Load aliases from $GIT_RC" >> "$HOME_RC"
+  echo "source $GIT_RC" >> "$HOME_RC"
 
-    echo "Added 'source $GIT_RC' to $HOME_RC."
+  echo "Added 'source $GIT_RC' to $HOME_RC."
 done
